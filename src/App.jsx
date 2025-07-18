1af077610c6de82837e6f53d7b9b909d2b026ba3
ndurushima <ndu1994@gmail.com>
2025-07-18 09:32:14 -0500

    Import BrowserRouter, Routes, and Route and define main routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+
+
 const App = () => {
     return (
         <>
-        {/* define all routes here */}
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </BrowserRouter>
         </>
     )
 }
